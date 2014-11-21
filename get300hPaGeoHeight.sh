9dfc67a2d612197205ca4ed84d0a7c12871091be
echarliewhite <charliewhite92@gmail.com>
2014-11-21 00:47:49 -0500

    added geopotential height script

---
+++ get300hPaGeoHeight.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# This code to be used on animus/sila
+
+dir="/climdata/ERAInterim/all_levels/dailymean/z/"
+files=$dir"*nc"
+# if directory changes change filename index
+namestart=44
+
+suffix="_NA_300hPa.nc"
+
+for file in $files
+do
+    filename=${file:$namestart}
+    shortname=${filename%.nc}
+    outputfile="/users/jk/14/cwhite/ERAInterim/dailymean/300hPa/z/$shortname$suffix" 
+    # may 1 is day 121, sept 30 is day 273
+    # 300 hPa
+    # longitude from -125 e to -70 e, latitude from 25 n to 60 n (North America)
+    ncks -d initial_time0_hours,121,273 -d g0_lon_3,-125.0,-70.0 -d g0_lat_2,25.0,60.0 -d lv_ISBL1,300.0 $file $outputfile
+    echo $outputfile
+done
