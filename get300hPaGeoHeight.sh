1b6a2f2cab537dee851727eed04b34124f3c491b
echarliewhite <charliewhite92@gmail.com>
2015-01-07 21:39:12 -0500

    getGeoHeight on whole circumference of earth

--- get300hPaGeoHeight.sh
+++ get300hPaGeoHeight.sh
@@ -16,7 +16,8 @@ do
     outputfile="/users/jk/14/cwhite/ERAInterim/dailymean/300hPa/z/$shortname$suffix" 
     # may 1 is day 121, sept 30 is day 273
     # 300 hPa
-    # longitude from -125 e to -70 e, latitude from 25 n to 60 n (North America)
+    # whole circumference (along line of latitude)
+    # latitude given by g0_lat_2
     ncks -d initial_time0_hours,121,273 -d g0_lat_2,40.0 -d lv_ISBL1,300.0 $file $outputfile
     echo $outputfile
 done
