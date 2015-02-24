2684752488eed3b6d1889d22021eca88854fb074
echarliewhite <charliewhite92@gmail.com>
2015-02-24 15:39:59 -0500

    geo height evolution added, old scripts removed

---
+++ geo_height_evolution.py
@@ -0,0 +1,86 @@
+"""
+File: geo_height_evolution.py
+Author: Charlie White
+Email: charlie.white@mail.utoronto.ca
+Github: echarliewhite
+Description: This script plots the evolution of the standing and travelling
+components of geopotential height for a specified heat wave.
+"""
+import numpy as np
+import heat_wave_tools as hwt
+import wnfreq_routines_2_0 as wnfreq
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+z_data, z_time, z_plev, z_lat, z_lon = hwt.import_nc_dir('/Users/charliewhite/Documents/Year4/Thesis/climdata/ERAInterim/dailymean/z/midlatitude/', 'Z_GDS0_ISBL', 300.0)
+
+t_data, t_time, t_plev, t_lat, t_lon = hwt.import_nc_dir('/Users/charliewhite/Documents/Year4/Thesis/climdata/ERAInterim/dailymean/t/summersurface/', 'T_GDS0_ISBL', 1000.0)
+
+# get heat wave data
+N_points = 0.05*t_lat.size*t_lon.size
+heat_wave_dict = hwt.find_heat_waves(t_data,97.5,t_lat,t_lon,N_points,5)
+
+z_clim = np.average(z_data, axis=0) # climatology
+# geoheight anomaly
+z_anom = z_data
+for i in range(z_data.shape[0]):
+    z_anom[i] -= z_clim
+
+wn_max = 15
+z_anom_trans, z_anom_trans_standing, z_anom_trans_travelling = \
+        wnfreq.calc_wnfreq_spectrum(z_anom, wn_max)
+z_anom_standing = wnfreq.invert_wnfreq_spectrum(
+        z_anom_trans_standing,1,wn_max,z_lon.size,tol=1e6)
+z_anom_travelling = wnfreq.invert_wnfreq_spectrum(
+        z_anom_trans_travelling,1,wn_max,z_lon.size,tol=1e6)
+
+# day0 for a heat wave
+hwave = heat_wave_dict.values()[1]
+min_lat_ind = np.where(z_lat==np.min(t_lat))[0][0]
+max_lat_ind = np.where(z_lat==np.max(t_lat))[0][0]
+min_lon_ind = np.where(z_lon==np.min(t_lon))[0][0]
+max_lon_ind = np.where(z_lon==np.max(t_lon))[0][0]
+z_anom_standing_day0 = z_anom_standing[hwave.year,hwave.start,0,
+        :,min_lon_ind:max_lon_ind+1]
+z_anom_travelling_day0 = z_anom_travelling[hwave.year,hwave.start,0,
+        :,min_lon_ind:max_lon_ind+1]
+
+# llcrnrlat,llcrnrlon,urcrnrlat,urcrnrlon
+# are the lat/lon values of the lower left and upper right corners
+# of the map.
+# resolution = 'c' means use crude resolution coastlines.
+m = Basemap(projection='cyl',llcrnrlat=np.min(t_lat),urcrnrlat=np.max(t_lat),\
+            llcrnrlon=np.min(t_lon),urcrnrlon=np.max(t_lon),resolution='c')
+# contour levels
+clevs = np.arange(np.min(z_anom_standing),np.max(z_anom_standing),
+        (np.max(z_anom_standing) - np.min(z_anom_standing))/100.0)
+# x,y grid from lat, lon
+lons, lats = np.meshgrid(t_lon,t_lat)
+x, y = m(lons, lats)
+# figure
+fig, ax = plt.subplots(1,2)
+# contours
+plt.sca(ax[0])
+cs = m.contour(x,y,z_anom_standing_day0,clevs,colors='k',linewidths=1.)
+plt.clabel(cs, inline=1, fontsize=10)
+m.drawcoastlines(linewidth=1.25)
+m.fillcontinents(color='0.5')
+# draw parallels and meridians.
+m.drawparallels(np.arange(np.min(t_lat),np.max(t_lat),5.),labels=[1,1,0,0,])
+m.drawmeridians(np.arange(np.min(t_lon),np.max(t_lon),10.),labels=[0,0,0,1])
+m.drawmapboundary()
+plt.title("Day 0 Standing Geo Height Anomaly")
+
+plt.sca(ax[1])
+cs = m.contour(x,y,z_anom_travelling_day0,clevs,colors='k',linewidths=1.)
+plt.clabel(cs, inline=1, fontsize=10)
+m.drawcoastlines(linewidth=1.25)
+m.fillcontinents(color='0.5')
+# draw parallels and meridians.
+m.drawparallels(np.arange(np.min(t_lat),np.max(t_lat),5.),labels=[1,1,0,0,])
+m.drawmeridians(np.arange(np.min(t_lon),np.max(t_lon),10.),labels=[0,0,0,1])
+m.drawmapboundary()
+plt.title("Day 0 Travelling Geo Height Anomaly")
+plt.show()
+
+import pdb; pdb.set_trace()
--- heat_wave_tools.py
+++ heat_wave_tools.py
@@ -9,21 +9,42 @@ on a threshold temperature defined relative to historical temperatures.
 import numpy as np
 import netCDF4 as nc4
 import os
+import string
 
 
-def import_temps(directory):
-    """This function will import the temperature data in the given directory,
-    put it all in one array, and return it along with arrays containing
-    longitude, latitude, pressure level, and date/time information."""
+class heat_wave:
+    """
+    A class for storing heatwaves and their properties.
+    """
+    def __init__(self, year, start, length, lat, lon):
+        self.year = year # year of the heat wave
+        self.start = start # starting date of the heat wave
+        self.length = length # number of days the heat wave lasted
+        self.lat = lat # average latitude of grid points above temp threshold
+        self.lon = lon # average longitude of grid points above temp threshold
+        
+
+def import_nc_dir(directory, var_name, pressure):
+    """
+    This function will import the data in the netcdf files in the given
+    directory, put it all in one array, and return it along with arrays
+    containing longitude, latitude, pressure level, and date/time information.
+    The variable of interest (e.g. geo height, temperature) must be specified
+    according to its name in the .nc file.
+    """
     
     files = os.listdir(directory)
-    var_name='T_GDS0_ISBL' #Temperature
+    # don't try to import non-netCDF files
+    for item in files:
+        if string.find(item, '.nc') == -1:
+            files.remove(item)
 
     # determine the shape of the data
     nc = nc4.Dataset(directory+files[0])
-    data = nc.variables[var_name][:]
-    time = nc.variables['initial_time0_hours'][:]
     plev = nc.variables['lv_ISBL1'][:]
+    p_ind = np.where(plev==pressure)[0]
+    data = nc.variables[var_name][:][:,p_ind,:,:]
+    time = nc.variables['initial_time0_hours'][:]
     lat = nc.variables['g0_lat_2'][:]
     lon = nc.variables['g0_lon_3'][:]
 
@@ -36,24 +57,28 @@ def import_temps(directory):
     # extract temperature data for each year and put in one array
     for i in range(len(files)):
         nc = nc4.Dataset(directory+files[i])
-        data[i] = nc.variables[var_name][:]
+        data[i] = nc.variables[var_name][:][:,p_ind,:,:]
         nc.close()
 
     # add in year as a returned variable
     return data, time, plev, lat, lon
     
-def find_heat_waves(temps,threshold,year,lat,lon,N_points,deg_move):
+
+def find_heat_waves(temps,threshold,lat,lon,N_points,deg_move):
     """
-    Finds heat waves in a given year based on a historical threshold
-    percentile at each date and location. Heat wave day is defined as one in
-    which the threshold temp is exceeded on that day and the following four
-    days in at least N_points locations/grid points. Assumes that temps has
-    structure [year,day,plev,lat,lon]. lat, lon define the latitudes/longitudes
+    Finds heat waves based on a historical threshold percentile at each date
+    and location. A heat wave day is defined as one in which the threshold temp
+    is exceeded on that day and the following four days in at least N_points
+    locations/grid points. Assumes that temps has structure
+    [year,day,plev,lat,lon]. lat, lon define the latitudes/longitudes
     represented in temps. N_points determines the number of points which must
     exceed the threshold temperature. deg_move determines how far the center of
     the hot locations may move from day to day in degrees latitude OR longitude
     (to prevent e.g. east coast heat followed coincidentally by west coast heat
     from being considered a single event.)
+
+    Returns a dictionary containing all heat waves. Each key is a heat wave
+    class, each entry may be used to contain plots of that heat wave.
     """
     
     # find threshold percentile value at each location/date
@@ -66,39 +91,55 @@ def find_heat_waves(temps,threshold,year,lat,lon,N_points,deg_move):
         highbound = min(temps_threshold.shape[0] - 1 - i, 7)
         temps_threshold[i,:,:,:] = \
           np.percentile(temps[:,i-lowbound:i+highbound,:,:,:],threshold,axis=(0,1))
-    hot_day_loc = np.greater(temps[year], temps_threshold)
-    hot_day = np.empty(temps.shape[1],dtype=bool)
-    for i in range(hot_day.size):
-        Nhot = np.flatnonzero(hot_day_loc[i]).size
-        if Nhot >= 10:
-            hot_day[i] = True
-        else:
-            hot_day[i] = False
-    heatwave = np.empty(hot_day.size,dtype=bool)
-    heatwave *= 0
-    # check for heatwave criteria on each day and subsequent days
-    for i in range(heatwave.size-4):
-        if hot_day[i]:
-            heatwave[i] = True
-            # locations where it is hot on day i
-            hot_day_indices = np.nonzero(hot_day_loc[i,0,:,:])
-            # average latitude and longitude of hot locations
-            lat_av = np.average(lat[hot_day_indices[0]])
-            lon_av = np.average(lon[hot_day_indices[1]])
 
-            for j in range(4):
-                if not hot_day[i+j+1]:
-                    heatwave[i] = False
+    heat_wave_dict = dict()
+
+    # find days that exceed the threshold value
+    hot_day_loc = np.empty(temps.shape, dtype=bool)
+    for i in range(temps.shape[0]): # iterate through years
+        # days and locations exceeding threshold temperature
+        hot_day_loc[i] = np.greater(temps[i], temps_threshold)
+        prev_day_heat_wave = False # bool indicating whether the day before was "hot"
+        for j in range(temps.shape[1]): # iterate through days
+            # hot grid points greater than specified threshold?
+            if np.flatnonzero(hot_day_loc[i][j]).size >= N_points:
+                if not prev_day_heat_wave: # possible heat wave start date
+                    # locations where it is hot
+                    hot_day_loc_indices = np.nonzero(hot_day_loc[i][j])
+                    # find center of hot locations
+                    lat_av = np.average(lat[hot_day_loc_indices[1]])
+                    lon_av = np.average(lon[hot_day_loc_indices[2]])
+                    
+                    k = 1
+                    if j < temps.shape[1] - 1:
+                        next_day_hot = True
+                    else: next_day_hot = False
+
+                    while next_day_hot:
+                        # is the next day "hot"?
+                        if np.flatnonzero(hot_day_loc[i][j+k]).size >= N_points:
+                            hot_day_loc_indices = np.nonzero(hot_day_loc[i][j+k])
+                            lat_av_next = np.average(lat[hot_day_loc_indices[1]])
+                            lon_av_next = np.average(lon[hot_day_loc_indices[2]])
+                            # does the center of the heat wave move too quickly?
+                            if (abs(lat_av_next - lat_av) > deg_move or
+                                  abs(lon_av_next - lon_av) > deg_move):
+                                next_day_hot = False
+                            else:
+                                k += 1
+                                lat_av, lon_av = lat_av_next, lon_av_next
+                        else: next_day_hot = False
+
+                    if k > 4: # sufficiently long string of hot days
+                        hot_day_loc_indices = np.nonzero(hot_day_loc[i][j])
+                        lat_av = np.average(lat[hot_day_loc_indices[1]])
+                        lon_av = np.average(lon[hot_day_loc_indices[2]])
+                        # store heat wave
+                        heat_wave_dict['year_%d_day_%d' % (i,j)] = heat_wave(i,j,k,lat_av,lon_av)
+                        prev_day_heat_wave = True
                 else:
-                    # check if center of hot days has moved by more than deg_move
-                    # degrees in lat or lon
-                    hot_day_indices = np.nonzero(hot_day_loc[i+j+1,0,:,:])
-                    lat_av_next = np.average(lat[hot_day_indices[0]])
-                    lon_av_next = np.average(lon[hot_day_indices[1]])
-                    # if i in [8,33,50]:
-                    #     import pdb; pdb.set_trace()
-                    if abs(lat_av_next - lat_av) > deg_move or abs(lon_av_next - lon_av) > deg_move:
-                        heatwave[i] = False
-                    lat_av, lon_av = lat_av_next, lon_av_next
+                    k -= 1
+                    # when heat wave ends start looking for heat waves again
+                    if k == 1: prev_day_heat_wave = False
+    return heat_wave_dict
 
-    return heatwave
