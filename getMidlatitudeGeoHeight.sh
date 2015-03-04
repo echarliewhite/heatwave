510c6ebbcd2f8d88ef950ae9f3bcaf21dfebee3c
echarliewhite <charliewhite92@gmail.com>
2015-03-04 14:10:02 -0500

    get whole northern hemisphere

--- getMidlatitudeGeoHeight.sh
+++ getMidlatitudeGeoHeight.sh
@@ -13,11 +13,11 @@ for file in $files
 do
     filename=${file:$namestart}
     shortname=${filename%.nc}
-    outputfile="/users/jk/14/cwhite/ERAInterim/dailymean/z/midlatitude/$shortname$suffix" 
+    outputfile="/users/jk/14/cwhite/ERAInterim/dailymean/z/NH/$shortname$suffix" 
     # may 1 is day 121, sept 30 is day 273
     # whole circumference (along lines of latitude)
     # latitude given by g0_lat_2
     # whole troposphere + lower stratosphere included
-    ncks -d initial_time0_hours,121,273 -d g0_lat_2,25.0,50.0 -d lv_ISBL1,50.0,1000.0 $file $outputfile
+    ncks -d initial_time0_hours,121,273 -d g0_lat_2,0.0,90.0 -d lv_ISBL1,50.0,1000.0 $file $outputfile
     echo $outputfile
 done
